173173758978e18a5101d749f339920457ac0b6c
lucassantana95-Dev <j.lucas.sa@hotmail.com>
2026-02-27 01:15:19 +0000

    Final desafio mestre

--- Desafio do xadrez.c
+++ Desafio do xadrez.c
@@ -1,5 +1,30 @@
 #include <stdio.h>
-   
+
+    void moverTorre(int casas){
+        if (casas > 0){
+        printf("Direita\n");
+        
+        moverTorre(casas - 1);
+ }}
+
+     void moverRainha(int casas){
+        if (casas > 0){
+        printf("Esquerda\n");
+        
+        moverRainha(casas - 1);
+ }}
+
+  void moverBispo(int casas){
+     if (casas > 0){
+        for (int i = 0; i < 1; i++){
+        for (int j = 0; j < 1; j++)
+        {
+             printf("Cima,Direita\n");
+        }}
+         
+        moverBispo(casas - 1);
+ }
+}
     int main(){
                     //Definição de movimento de cada peca
         int movimentoTorre = 5, movimentoBispo = 5, movimentoRainha = 8;
@@ -7,46 +32,40 @@
         // Movimento da Torre(for)
         // Mover 5 casas para a direita.
         printf("Movimento da Torre:\n");
-        for (int i = 1; i <= movimentoTorre; i++){
-            printf("Direita\n");
-        }
-        printf("\n"); //pular uma linha para saber o proximo passo
+            moverTorre(movimentoTorre);
 
+            printf("\n");
+            
         // Movimento do Bispo(while)
         // O Bispo move 5 casas na diagonal a direita(cima/direita)
         printf("Movimento do Bispo:\n");
-        int b = 1;
-        while (b <= movimentoBispo) {
-            printf("Cima, direita\n");
-            b++; //colocar sempre para acrescentar +1 e chegar ao valor definido de vezes
-        } 
+            moverBispo(movimentoBispo);
+        
         printf("\n");
 
         // Movimento da Rainha(do_while)
         // A rainha move 8 casas para a esquerda
         printf("Movimento da Rainha:\n");
-        int r =1;
-        do{
-            printf("Esquerda\n");
-            r++;//colocar sempre para acrescentar +1 e chegar ao valor definido de vezes
-        }while (r <= movimentoRainha);
+            moverRainha(movimentoRainha);
+      
         printf("\n");
-         
+
         // Movimento do cavalo(loop aninhados)
         // Mover 2 para baixo uma para esquerda.
         printf("Movimento do cavalo:\n");
-        for (int j = 1; j <= 2; j++){
-            printf("Baixo\n");
-            int k = 1;
-            if(j == 2)// coloca 2 '=' para poder comparar
-           
-            while (k == 1){         //executa apenas 1 vez
-                printf("Esquerda\n");
-                k++; }
+        for (int i = 1, j = 1; i <= 2; i++) {
+        printf("Cima\n");
         
-         }
-        printf("\n");
+        // Quando atingir a 2ª casa para cima, faz a curva para a direita
+        if (i == 2) {
+            while (j <= 1) {
+                printf("Direita\n");
+                j++; // Incremento para sair do while
+            }
+        }
+    }
 
         return 0;
+
 
     }
